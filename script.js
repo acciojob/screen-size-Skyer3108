2e750a118242c30b85721c54ac3caa0469973166
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 15:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+
+function updateSize() {
+    
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+
+    const sizeInfoElement = document.querySelector('#sizeInfo h1');
+    
+   
+    sizeInfoElement.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+
+updateSize();
+
+
+window.addEventListener('resize', updateSize);
